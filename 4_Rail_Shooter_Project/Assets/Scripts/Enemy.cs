cb8e7a0f393dce6de39eedd5a64f6e8b2d23dca1
mattmedlin <matthewkmedlin94@gmail.com>
2020-11-16 15:04:36 -0600

    scoreboard

--- 4_Rail_Shooter_Project/Assets/Scripts/Enemy.cs
+++ 4_Rail_Shooter_Project/Assets/Scripts/Enemy.cs
@@ -7,10 +7,15 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
+
+    ScoreBoard scoreBoard;
+
     // Start is called before the first frame update
     void Start()
     {
         AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     private void AddNonTriggerBoxCollider()
@@ -21,6 +26,7 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit(scorePerHit);
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
